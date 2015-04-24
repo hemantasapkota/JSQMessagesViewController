d52ecfa3a5ecf2bacd7b89429ec7928d0e0a5d9e
Hemanta Sapkota <laex.pearl@gmail.com>
2015-04-24 21:35:09 +1000

    Squashed 'JSQSystemSoundPlayer/' content from commit 2d8a49b
    git-subtree-dir: JSQSystemSoundPlayer
    git-subtree-split: 2d8a49bbb6490c05f45f7feb6edd32655e18169d

---
+++ SoundPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  The MIT License
+//  Copyright (c) 2014 Jesse Squires
+//  http://opensource.org/licenses/MIT
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
